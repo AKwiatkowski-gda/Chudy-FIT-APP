20818ea58cf24bc2f38d50be5463b45655106db6
Arek <gdx164241@student.gdansk.merito.pl>
2024-11-28 17:22:13 +0100

    zajęcia 28.11.2024

--- chudyapp/views.py
+++ chudyapp/views.py
@@ -1,3 +1,13 @@
+from django.core import serializers
 from django.shortcuts import render
+from django.http import HttpResponse
+from chudyapp.models.PersonData import PersonData
 
+def indexArticleJson(request):
+    articles = PersonData.objects.all()
+    data = serializers.serialize('json', articles)
+    return HttpResponse(data, content_type="applicaton/json")
+
+def index(request):
+    return HttpResponse("hello world")
 # Create your views here.
